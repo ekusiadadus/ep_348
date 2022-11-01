5f478175ecf9fa3c2343e7fe334090fdfebb8846
ekusiadadus <ekusiadadus@gmail.com>
2022-11-01 12:05:46 +0900

    chore: :world: pointer_type

--- main.cpp
+++ main.cpp
@@ -2,18 +2,23 @@ void lambda() {
   auto l = []([[maybe_unused]] int value, int &value2) -> long {
     return ++value + value2++;
   };
-  int i = 13;
-  l(42, i);
+  long (*fp)(int, int &) = l;
 }
 
 struct Lambda {
+  using pointer_type = long (*)(int, int &);
+  constexpr static auto FUNC([[maybe_unused]] int value, int &value2) -> long {
+    return ++value + value2++;
+  }
+public:
   constexpr auto operator()([[maybe_unused]] int value, int &value2) const -> long {
     return ++value + value2++;
   }
+
+  constexpr operator pointer_type() { return FUNC; }
 };
 
 void equivalent() {
   auto l = Lambda{};
-  int i = 13;
-  l(42, i);
+  long (*fp)(int, int &) = l;
 }
